0415c3739629338070b7b7a5d880c3d8dac97f18
Andre <none@no_server.com>
2016-11-21 16:04:56 +0000

    Adapted scripts to dir changes

--- compileServer.sh
+++ compileServer.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+mkdir -p build
+
 slice2cpp PortalInterface.ice
 g++ -I. -c PortalInterface.cpp Portal.cpp # -std=c+11
-g++ -o portal PortalInterface.o Portal.o -lIce -lIceUtil -lpthread
+g++ -o build/portal PortalInterface.o Portal.o -lIce -lIceUtil -lpthread
 g++ -I. -c PortalInterface.cpp Streamer.cpp
-g++ -o streamer PortalInterface.o Streamer.o -lIce -lIceUtil -lpthread
+g++ -o build/streamer PortalInterface.o Streamer.o -lIce -lIceUtil -lpthread
